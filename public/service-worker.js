3719d2ee768c067efff8e9c939d01af81bad9097
Saraheve29 <sarahwhyte33@gmail.com>
2026-07-05 23:47:10 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,18 @@
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
+      Promise.all(keys.map(key => {
+        console.log('Deleting cache:', key);
+        return caches.delete(key);
+      }))
+    ).then(() => self.clients.claim())
   );
 });
+
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request.clone()).catch(() => caches.match(event.request)));
 });
